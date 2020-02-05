9f2824388845e28b503be2d5ce7d0c2606981261
Jingying Huang <wphuang@ucdavis.edu>
2020-02-04 23:04:21 -0800

    add resource page

--- app/src/main/java/com/belonging/developer/resources_activity.java
+++ app/src/main/java/com/belonging/developer/resources_activity.java
@@ -8,6 +8,6 @@ public class resources_activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_resources_activity);
+        setContentView(R.layout.activity_resources);
     }
 }
